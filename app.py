7508c6cccd7c71343ccfc095dc0117c35a15cc3f
Iathi <98141620+Iathi@users.noreply.github.com>
2025-04-02 22:24:52 -0300

    Update app.py

--- app.py
+++ app.py
@@ -8,6 +8,6 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-if __name__ == "__main__":
-    threading.Thread(target=bot.run_bot).start()  # Inicia o bot em paralelo
-    app.run(debug=True, host="0.0.0.0", port=5000)
+if __name__ == '__main__':
+    app.run(debug=True)
+            
